d6d8814907ac5e4d073d38b41a155a7cc1f4f53f
Hanhhh05 <94811980+Hanhhh05@users.noreply.github.com>
2021-11-22 19:34:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    titleWeb: "Gửi cho homie của tui<3",
+    titleWeb: "Gửi cho homie <3",
     introTitle: 'Homie oiii:33',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao homie cứ đi nhầm đường
-    Lạc vào tim toy lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích emm <33',
-    btnNo: 'Không, em à :3',
+    introDesc: `Homie có phải là GPS mang toy vào trái tim anh khongg??`,
+    btnIntro: 'Haha',
+    title: 'Gifchi gifchi goo',
+    desc: 'means that i love u ',
+    btnYes: 'oki nuông',
+    btnNo: ' Không',
     question: 'whyyyy',
     btnReply: 'Gửi cho homie <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'toy biết mà 🥰. Yêu homie nhiều nhắm :33',
+    reply: 'biet ban tay nay muon cham vao ai roi',
+    mess: 'biet trong dau nay chi co ai do thoi',
     messDesc: 'thế thì ib cho nhao nào:333',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100042336033959' //link mess của các bạn. VD: https://m.me/nam.nodemy
